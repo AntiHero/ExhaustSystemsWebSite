3e4f949a3506bc631a7034043bdc1169d03ec467
AntiHero <prophreaker@gmail.com>
2019-08-18 18:07:50 +0300

    gallery slider add(feat)

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,4 +17,21 @@ $( document ).ready(function() {
       scrollTop: $('#examples').offset().top + 'px'
     }, 10);
   });
+  
+  let swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
 });
